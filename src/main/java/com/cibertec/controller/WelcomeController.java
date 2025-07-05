7a342c969d0e7e5f3de7f55d7845c157f69c6ac8
Soraya-cam <i202318552@cibertec.edu.pe>
2025-07-04 20:49:34 -0500

    Add files via upload

---
+++ src/main/java/com/cibertec/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.cibertec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+	
+	
+
+}
